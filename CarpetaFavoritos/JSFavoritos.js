89b22865683c44af312fca7f8a9edbe916ab4335
Matías Páez <matiaspaezws@gmail.com>
2024-09-29 14:38:51 -0300

    arreglos de carrousel en perfil y favoritos

---
+++ CarpetaFavoritos/JSFavoritos.js
@@ -0,0 +1,9 @@
+//usamos un query selector con data-background-image para obtener la imagen actual de cada
+//carrousel. Despues aplicamos estilos desde este JS, no se pudo encontrar otra solución por
+//el momento
+
+document.querySelectorAll('.slide').forEach(function (slide) {
+    var imgSrc = slide.getAttribute('data-background-image');
+    slide.style.setProperty('--background-image', `url(${imgSrc})`);
+    slide.style.backgroundImage = `url(${imgSrc})`;
+});
