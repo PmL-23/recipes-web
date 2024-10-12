6ae2cd595cddd28629a98a4f626352c79d593a78
Matías Páez <matiaspaezws@gmail.com>
2024-10-12 18:11:54 -0300

    seccion Perfil - avance JS

--- CarpetaPerfil/Perfil.php
+++ CarpetaPerfil/Perfil.php
@@ -5,10 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Recetario</title>
         
+<<<<<<< HEAD:CarpetaPerfil/Perfil.php
         
         <link rel="stylesheet" href="../CarpetaPerfil/EstilosPerfil.css">
         <link rel="stylesheet" href="../CarpetaFavoritos/EstilosFavoritos.css">
         <script src="../CarpetaPerfil/JSPerfil.js" defer></script>
+=======
+        <script src="./JSPerfil.js" defer></script>
+
+>>>>>>> 84cbfc1 (seccion Perfil - avance JS):CarpetaPerfil/Perfil.html
         
         <?php include '../includes/head.php'?>
         
@@ -219,107 +224,20 @@ Dios te ayude hermano
 -->
 <hr class="mt-0">
 <br>
-    <div class="container-fluid row ">
-        <div class="col-1 "></div>
-
-        <div class=" col-10 BoxPublicacion p-0">
-            <div class="card" >
-                <div class=" DivEncabezadoPublicacion ">
-                    <img class="d-inline " alt="Texto si no ve imagen" src="../images/bondiola_lp.jpg"width="25" height="25">
-                    <p class="d-inline" id="IDNombreCompletoDeUsuarioEnPublicacion">Nombre & Apellido de Usuario</p>
-                    <p class="d-inline text-secondary" id="IDNombreDeUsuarioEnPublicacion">@NombreDeUsuario</p>
-                </div>
-
-                <div id="carouselExampleIndicators" class="carousel slide carouselPerfil">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        
-                    </div>
-                    <div class="carousel-inner"> <!---buscar manera para que el tamaño de una imagen no perjudique la UX-->
-                        <div class="carousel-item active slide" data-background-image="../images/bondiola_lp.jpg">
-                            <img src="../images/bondiola_lp.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item slide" data-background-image="../images/asado_lp.webp">
-                            <img src="../images/asado_lp.webp" class="d-block w-100 " alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-                
-
-                <div class="card-body ">
-                    <h5 class="card-title ">Titulo Publicacion</h5>
-                    <p class="card-text">Descripcion...................</p>
-                    <p class="card-text">Descripcion...................</p>
-                    <p class="card-text">Descripcion...................</p>
-                    <p class="card-text">Descripcion...................</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <p class="card-text EstilosCategorias">Categoria</p>
-                        <p class="card-text EstilosEstiquetas">Estiqueta1</p>
-                        <p class="card-text EstilosEstiquetas">Estiqueta2</p>
-                        <p class="card-text EstilosEstiquetas">Estiqueta3</p>
-                        <p class="card-text EstilosEstiquetas">Estiqueta4</p>
-                        <p class="card-text EstilosEstiquetas">Estiqueta5</p>
-                    </li>
-                    <li class="list-group-item">
-                        <ul>
-                            <li><p class="card-text">Ingrediente</p></li>
-                            <li><p class="card-text">Ingrediente</p></li>
-                            <li><p class="card-text">Ingrediente</p></li>
-                            <li><p class="card-text">Ingrediente</p></li>
-                        </ul>
-                    </li>
-                </ul>
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <!-- Valoración y cantidad de valoraciones -->
-                        <div class="valoracion">
-                            <p>Valoración: ★★★★☆ (32 valoraciones)</p>
-                        </div>
-                        
-                        <!-- Cantidad de comentarios -->
-                        <div class="comentarios">
-                            <p>10 comentarios</p>
-                        </div>
-                    </div>
-                    
-                    <!-- Botones de acción -->
-                    <div class="d-flex justify-content-end mt-2">
-                        <!-- Botón para compartir -->
-                        <button class="btn btn-outline-primary me-2" type="button">
-                            Compartir
-                        </button>
-                
-                        <!-- Botón para guardar en favoritos -->
-                        <button class="btn btn btn-outline-dark" type="button">
-                            Guardar en Favoritos
-                        </button>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-
-        <div class="col-1 "></div>
+<br>
+<br>
+<br>
+    <div class="" id="IDContenedorPublicacionesPropias">
 
     </div>
+
+
     <br>
-    <hr>
     <br>
         <div class="container-fluid row ">
             <div class="col-1 "></div>
     
-            <div class=" col-10 BoxPublicacion p-0">
+            <div class=" col-10 p-0">
                 <div class="card" >
                     <div class=" DivEncabezadoPublicacion ">
                         <img class="d-inline " alt="Texto si no ve imagen" src="../images/bondiola_lp.jpg"width="25" height="25">
