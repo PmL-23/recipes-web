f1b86f5aafc163ec13e164edb3d7c5b1ade5d3db
ch-unnamed <pepe.n.acuna@gmail.com>
2024-10-21 21:52:16 -0300

    new: Recuperar cuenta/cambiar contraseña. add: phpmailer/composer.json

---
+++ inicio_sesion/recuperar_cuenta.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$loginOK = null; 
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar contraseña</title>
+    <?php include '../includes/head.php' ?>
+</head>
+
+<body>
+    <?php include '../includes/header.php' ?>
+    <h1>COMPLETE EL SIGUIENTE FORMULARIO</h1>
+    <BR>
+<form action="enviar_token.php" method="POST">
+    <label for="email">Ingresa tu correo electrónico:</label>
+    <input type="email" id="email" name="email" required>
+    <button type="submit">Recuperar Contraseña</button>
+</form>
+</body>
+</html>
