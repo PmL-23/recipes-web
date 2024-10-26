22df5e98fc69875dbd118c0a11f696a9dd00b767
ch-unnamed <pepe.n.acuna@gmail.com>
2024-10-26 01:13:04 -0300

    Actualización de queries en eliminar_publicacion.

--- perfil-usuario/eliminar_publicacion.php
+++ perfil-usuario/eliminar_publicacion.php
@@ -11,13 +11,29 @@ if (!isset($_SESSION['id'])) {
 if (isset($_POST['id'])) {
     $id_publicacion = intval($_POST['id']);
     
+    
     try {
-        $query = "DELETE FROM publicaciones_recetas WHERE id_publicacion = :id_publicacion AND id_usuario_autor = :id_usuario_autor";
-        $stm = $conn->prepare($query); 
-        $stm->bindParam(':id_publicacion', $id_publicacion, PDO::PARAM_INT);
-        $stm->bindParam(':id_usuario_autor', $_SESSION['id'], PDO::PARAM_INT);
+        $queries = [
+            "DELETE FROM ingredientes_recetas WHERE id_publicacion = :id_publicacion",
+            "DELETE FROM etiquetas_recetas WHERE id_publicacion = :id_publicacion",
+            "DELETE FROM paises_recetas WHERE id_publicacion = :id_publicacion",
+            "DELETE FROM valoraciones WHERE id_publicacion = :id_publicacion",
+            "DELETE FROM imagenes_pasos WHERE id_paso IN (SELECT id_paso FROM pasos_recetas WHERE id_publicacion = :id_publicacion)",
+            "DELETE FROM pasos_recetas WHERE id_publicacion = :id_publicacion"
+        ];
+    
+        foreach ($queries as $query) {
+            $stm = $conn->prepare($query);
+            $stm->bindParam(':id_publicacion', $id_publicacion, PDO::PARAM_INT);
+            $stm->execute();
+        }
 
-        if ($stm->execute()) {
+        $queryPublicacion = "DELETE FROM publicaciones_recetas WHERE id_publicacion = :id_publicacion AND id_usuario_autor = :id_usuario_autor";
+        $publicacionStm = $conn->prepare($queryPublicacion); 
+        $publicacionStm->bindParam(':id_publicacion', $id_publicacion, PDO::PARAM_INT);
+        $publicacionStm->bindParam(':id_usuario_autor', $_SESSION['id'], PDO::PARAM_INT);
+
+        if ($publicacionStm->execute()) {
             echo json_encode(['status' => 'success', 'message' => 'Publicación eliminada correctamente.']);
         } else {
             echo json_encode(['status' => 'error', 'message' => 'Error al eliminar la publicación.']);
@@ -29,3 +45,4 @@ if (isset($_POST['id'])) {
     echo json_encode(['status' => 'error', 'message' => 'ID de publicación no proporcionado.']);
 }
 ?>
+
