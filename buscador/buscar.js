1a2f28fa4018fdd438a7d5242deecd4345b3029e
kodak-code <mamanicamaramatias@industrialdetemperley.edu.ar>
2024-10-27 11:44:05 -0300

    chau buscador

--- buscador/buscar.js
+++ buscador/buscar.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    let buscarReceta = document.getElementById('buscarReceta'); //buscador que tenemos como form
+    let buscarReceta = document.getElementById('buscarReceta'); 
     let pantallasChicas = document.getElementById('pantallasChicas');
     let pantallaGrande = document.getElementById('pantallaGrande');
     let modalFiltros = document.getElementById('modalFiltros');
     let cerrarModal = document.querySelector('.close-modal');
     let resultados = document.getElementById("resultados");
+    let botonFiltro = document.querySelector('.filtrar-btn'); 
 
     buscarReceta.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        let formData = new FormData(this); //datos del formulario
+        let formData = new FormData(this); 
 
         fetch('recetas.php', {
             method: 'POST',
@@ -18,20 +18,22 @@ document.addEventListener("DOMContentLoaded", function () {
         })
             .then(response => response.text())
             .then(data => {
-                resultados.innerHTML = data; // resultados en el contenedor
-                // Depuración en la consola para ver el contenido recibido
+                resultados.innerHTML = data;
                 console.log("Contenido recibido:", data);
                 if (data.trim() !== "") { 
                     pantallaGrande.classList.remove('oculto');
-                    console.log("pantallaGrande se muestra"); // Verificar en la consola
+                    console.log("pantallaGrande se muestra");
                 }
             })
             .catch(err => console.log('Error:', err));
-
-
     });
-    
-    //modal
+
+    // Mostrar filtro para pantallas grandes al hacer clic en el botón de filtro
+    botonFiltro.addEventListener('click', function () {
+        pantallaGrande.classList.toggle('oculto'); // Muestra/oculta el div pantallaGrande
+    });
+
+    // modal
     pantallasChicas.addEventListener('click', function (event) {
         event.preventDefault(); 
         modalFiltros.classList.remove('oculto'); 
@@ -40,11 +42,10 @@ document.addEventListener("DOMContentLoaded", function () {
         modalFiltros.classList.add('oculto'); 
     });
 
-    // cerrar el modal al hacer clic fuera del contenido del modal
+    // Cerrar el modal al hacer clic fuera del contenido del modal
     window.addEventListener('click', function (event) {
         if (event.target === modalFiltros) {
             modalFiltros.classList.add('oculto'); 
         }
     });
-
 });
