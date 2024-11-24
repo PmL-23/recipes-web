7c1c9d52709479490f781a4ee792a8f86e98798f
Matías Páez <matiaspaezws@gmail.com>
2024-11-24 14:06:04 -0300

    validacion de largo en cambiar contraseña

--- perfil-usuario/editar_contraseña.js
+++ perfil-usuario/editar_contraseña.js
@@ -117,14 +117,23 @@ document.getElementById("FormCambiarContraseña").addEventListener("submit", fun
     ConfirmaciónNuevaContraseñaError.textContent = "";
 
     let bandera = 0;
-    if (ContraseñaActual.length <= 4) {
-        ContraseñaActualError.textContent = "La contraseña debe tener al menos 5 caracteres.";
+    if (ContraseñaActual.length <= 7) {
+        ContraseñaActualError.textContent = "La contraseña debe tener al menos 8 caracteres.";
         bandera += 1;
     }
 
+    if (NuevaContraseña.length <= 7) {
+        NuevaContraseñaError.textContent = "La contraseña debe tener al menos 8 caracteres.";
+        bandera += 1;
+    }
 
-    if (NuevaContraseña.length <= 4) {
-        NuevaContraseñaError.textContent = "La contraseña debe tener al menos 5 caracteres.";
+    if (ContraseñaActual.length >= 17) {
+        ContraseñaActualError.textContent = "La contraseña debe tener un maximo de  16 caracteres.";
+        bandera += 1;
+    }
+
+    if (NuevaContraseña.length >= 17) {
+        NuevaContraseñaError.textContent = "La contraseña debe tener un maximo de  16 caracteres.";
         bandera += 1;
     }
 
